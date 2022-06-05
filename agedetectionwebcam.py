d1367eb708360c89327e5906c2e94d7481a4480f
Dhruv237 <78265326+Dhruv237@users.noreply.github.com>
2022-06-05 21:59:57 +0530

    Add files via upload

---
+++ agedetectionwebcam.py
@@ -0,0 +1,95 @@
+import face_recognition
+import numpy as np
+import argparse
+import cv2
+import os
+from os import listdir
+# construct the argument parse and parse the arguments
+folder = r"C:/Program Files/projectimg"
+ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required=True,
+#                help="path to input image")
+ap.add_argument("-f", "--face", required=True,
+                help="path to face detector model directory")
+ap.add_argument("-a", "--age", required=True,
+                help="path to age detector model directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)",
+               "(38-43)", "(48-53)", "(60-100)"]
+print("[INFO] loading face detector model...")
+prototxtPath = 'C:/Users/Dell/deploy.prototext.txt'
+weightsPath = 'C:/Users/Dell/res10_300x300_ssd_iter_140000.caffemodel'
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+# load our serialized age detector model from disk
+print("[INFO] loading age detector model...")
+prototxtPath = 'C:/Users/Dell/age_deploy.prototext.txt'
+weightsPath = 'C:/Users/Dell/age_net.caffemodel'
+ageNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+#image = cv2.imread(args["image"])
+#image = cv2.resize(image, (720, 640))
+cap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    "haarcascade_frontalface_default.xml")
+while True:
+    ret, image = cap.read()
+    imgGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        roi_color = image[y:y + h, x:x + w]
+        cv2.imwrite(str(w) + str(h) + '_faces.png', roi_color)
+        try:
+           # unknown_picture = face_recognition.load_image_file(
+           #     'C:/Users/Dell/' + str(w) + str(h) + '_faces.png')
+            image=cv2.imread('C:/Users/Dell/' + str(w) + str(h) + '_faces.png')
+            #image = cv2.cvtColor(unknown_picture, cv2.COLOR_BGR2RGB)
+            (h, w) = image.shape[:2]
+            blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+                                         (104.0, 177.0, 123.0))
+            # pass the blob through the network and obtain the face
+            # detections
+            print("[INFO] computing face detections...")
+            faceNet.setInput(blob)
+            detections = faceNet.forward()
+            for i in range(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with the
+                # prediction
+                confidence = detections[0, 0, i, 2]
+                # filter out weak detections by ensuring the confidence is
+                # greater than the minimum confidence
+                if confidence > args["confidence"]:
+                    # compute the (x, y)-coordinates of the bounding box for the
+                    # object
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("uint8")
+                    # extract the ROI of the face and then construct a blob from
+                    # *only* the face ROI
+                    face = image[startY:endY, startX:endX]
+                    faceBlob = cv2.dnn.blobFromImage(
+                        face, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+                ageNet.setInput(faceBlob)
+                preds = ageNet.forward()
+                i = preds[0].argmax()
+                age = AGE_BUCKETS[i]
+                ageConfidence = preds[0][i]
+                # display the predicted age to our terminal
+                text = "{}: {:.2f}%".format(age, ageConfidence * 100)
+                print("[INFO] {}".format(text))
+                # draw the bounding box of the face along with the associated
+                # predicted age
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(image, (startX, startY), (endX, endY),
+                              (0, 0, 255), 2)
+                cv2.putText(image, text, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        except:
+            print("image problem")
+    cv2.imshow("face_detect", image)
+    cv2.waitKey(0)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyWindow('face_detect')
