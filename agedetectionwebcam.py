90b62e01458cefbbae54e7929b9c356b5e9a72f4
Dhruv237 <78265326+Dhruv237@users.noreply.github.com>
2022-06-06 15:54:41 +0530

    Add files via upload

--- agedetectionwebcam.py
+++ agedetectionwebcam.py
@@ -1,9 +1,35 @@
-import face_recognition
+#import face_recognition
 import numpy as np
 import argparse
 import cv2
 import os
 from os import listdir
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 # construct the argument parse and parse the arguments
 folder = r"C:/Program Files/projectimg"
 ap = argparse.ArgumentParser()
@@ -19,20 +45,18 @@ args = vars(ap.parse_args())
 AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)",
                "(38-43)", "(48-53)", "(60-100)"]
 print("[INFO] loading face detector model...")
-prototxtPath = 'C:/Users/Dell/deploy.prototext.txt'
-weightsPath = 'C:/Users/Dell/res10_300x300_ssd_iter_140000.caffemodel'
+prototxtPath = '/home/dlinano/real-time-age-detection-using-webcam-main/deploy.prototext.txt'
+weightsPath = '/home/dlinano/real-time-age-detection-using-webcam-main/res10_300x300_ssd_iter_140000.caffemodel'
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 # load our serialized age detector model from disk
 print("[INFO] loading age detector model...")
-prototxtPath = 'C:/Users/Dell/age_deploy.prototext.txt'
-weightsPath = 'C:/Users/Dell/age_net.caffemodel'
+prototxtPath = '/home/dlinano/real-time-age-detection-using-webcam-main/age_deploy.prototext.txt'
+weightsPath = '/home/dlinano/real-time-age-detection-using-webcam-main/age_net.caffemodel'
 ageNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 #image = cv2.imread(args["image"])
 #image = cv2.resize(image, (720, 640))
-cap = cv2.VideoCapture(0)
-faceCascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades +
-    "haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades +"haarcascade_frontalface_default.xml")
 while True:
     ret, image = cap.read()
     imgGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -44,8 +68,8 @@ while True:
         try:
            # unknown_picture = face_recognition.load_image_file(
            #     'C:/Users/Dell/' + str(w) + str(h) + '_faces.png')
-            image=cv2.imread('C:/Users/Dell/' + str(w) + str(h) + '_faces.png')
-            #image = cv2.cvtColor(unknown_picture, cv2.COLOR_BGR2RGB)
+            image=cv2.imread(str(w) + str(h) + '_faces.png')
+            image = cv2.cvtColor(unknown_picture, cv2.COLOR_BGR2RGB)
             (h, w) = image.shape[:2]
             blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
                                          (104.0, 177.0, 123.0))
@@ -64,12 +88,11 @@ while True:
                     # compute the (x, y)-coordinates of the bounding box for the
                     # object
                     box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("uint8")
+                    (startX, startY, endX, endY) = box.astype("int")
                     # extract the ROI of the face and then construct a blob from
                     # *only* the face ROI
                     face = image[startY:endY, startX:endX]
-                    faceBlob = cv2.dnn.blobFromImage(
-                        face, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+                    faceBlob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
                 ageNet.setInput(faceBlob)
                 preds = ageNet.forward()
                 i = preds[0].argmax()
